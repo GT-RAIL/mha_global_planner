d8f99270f71f0eb59cfc9d1ae6035dffe929616e
Peter Mitrano <mitranopeter@gmail.com>
2016-06-06 18:23:56 -0400

    removed demo path

--- src/mha_global_planner.cpp
+++ src/mha_global_planner.cpp
@@ -21,22 +21,14 @@ void MhaGlobalPlanner::initialize(std::string name,
 bool MhaGlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start,
                                 const geometry_msgs::PoseStamped& goal,
                                 std::vector<geometry_msgs::PoseStamped>& plan) {
-    plan.push_back(start);
-   for (int i=0; i<20; i++){
-     geometry_msgs::PoseStamped new_goal = goal;
-     tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+  plan.push_back(start);
 
-      new_goal.pose.position.x = -2.5+(0.05*i);
-      new_goal.pose.position.y = -3.5+(0.05*i);
+  //...
+  //plan.push_back(waypoints);
+  //...
 
-      new_goal.pose.orientation.x = goal_quat.x();
-      new_goal.pose.orientation.y = goal_quat.y();
-      new_goal.pose.orientation.z = goal_quat.z();
-      new_goal.pose.orientation.w = goal_quat.w();
+  plan.push_back(goal);
 
-   plan.push_back(new_goal);
-   }
-   plan.push_back(goal);
   return true;
 }
 };
