69bf6ea773a1fe8ba673be79c688d257f047558d
Peter Mitrano <mitranopeter@gmail.com>
2016-06-06 17:07:12 -0400

    First demo of example planner from wiki
    added launch files to use this planner
    instead of using turtlebot_gazebo amcl.launch, use mha_global_planner amcl.launch

--- include/mha_global_planner/mha_global_planner.h
+++ include/mha_global_planner/mha_global_planner.h
@@ -1,1 +1,26 @@
+#pragma once
+
 #include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
+
+namespace mha_global_planner {
+
+class MhaGlobalPlanner : public nav_core::BaseGlobalPlanner {
+ public:
+  MhaGlobalPlanner();
+  MhaGlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+  virtual bool makePlan(const geometry_msgs::PoseStamped& start,
+                        const geometry_msgs::PoseStamped& goal,
+                        std::vector<geometry_msgs::PoseStamped>& plan);
+
+  virtual void initialize(std::string name,
+                          costmap_2d::Costmap2DROS* costmap_ros);
+};
+};
--- src/mha_global_planner.cpp
+++ src/mha_global_planner.cpp
@@ -1,3 +1,42 @@
 #include "mha_global_planner/mha_global_planner.h"
 
-int main(int argc, char **argv) {}
+#include <pluginlib/class_list_macros.h>
+
+// register plugin
+PLUGINLIB_EXPORT_CLASS(mha_global_planner::MhaGlobalPlanner,
+                       nav_core::BaseGlobalPlanner)
+
+namespace mha_global_planner {
+
+MhaGlobalPlanner::MhaGlobalPlanner() {}
+
+MhaGlobalPlanner::MhaGlobalPlanner(std::string name,
+                                   costmap_2d::Costmap2DROS* costmap_ros) {
+  initialize(name, costmap_ros);
+}
+
+void MhaGlobalPlanner::initialize(std::string name,
+                                  costmap_2d::Costmap2DROS* costmap_ros) {}
+
+bool MhaGlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                                const geometry_msgs::PoseStamped& goal,
+                                std::vector<geometry_msgs::PoseStamped>& plan) {
+    plan.push_back(start);
+   for (int i=0; i<20; i++){
+     geometry_msgs::PoseStamped new_goal = goal;
+     tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+
+      new_goal.pose.position.x = -2.5+(0.05*i);
+      new_goal.pose.position.y = -3.5+(0.05*i);
+
+      new_goal.pose.orientation.x = goal_quat.x();
+      new_goal.pose.orientation.y = goal_quat.y();
+      new_goal.pose.orientation.z = goal_quat.z();
+      new_goal.pose.orientation.w = goal_quat.w();
+
+   plan.push_back(new_goal);
+   }
+   plan.push_back(goal);
+  return true;
+}
+};
